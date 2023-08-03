0918327ae4a5b5c476d943575c8afc35d3d22cfb
Ta0Huu <anuwatch6552@gmail.com>
2023-08-03 15:21:59 +0700

    saved

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,13 +1,17 @@
 import { ThemeContext } from "@/contexts/ThemeContext";
+import { LangContext } from "@/contexts/LangContext";
 import { useContext } from "react";
 
 export const Detail = () => {
+  const { lang } = useContext(LangContext);
   const { theme } = useContext(ThemeContext);
   return (
     <p className="text-center" style={{ color: theme.fgColor }}>
-      Display activated theme message here (EN or TH)
+      {theme.name === "light"
+        ? lang.detail.lightActivated
+        : lang.detail.darkActivated}
       {/* tip: Get information from "theme" variable to determine 
-      which theme is currently selected */}
+      which theme is currently selected Display activated theme message here (EN or TH)*/}
     </p>
   );
 };
